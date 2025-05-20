26d2a4f4addfe1c27ef671f9dba97c30a20c71fd
Christian Vase Frederiksen <chf004@edu.zealand.dk>
2025-05-20 12:50:14 +0200

    fix

--- Dyreinternat_Console/BlogTest.cs
+++ Dyreinternat_Console/BlogTest.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dyreinternat_Library;
+using Dyreinternat_Library.Models;
 using Dyreinternat_Library.Services;
 
 namespace Dyreinternat_Console
@@ -20,9 +22,7 @@ namespace Dyreinternat_Console
         public void BlogTests() // Test for all blogs
         {
             Console.WriteLine("Muligheder til blog");
-            Console.WriteLine("1. Opret en blog");
-            Console.WriteLine("2. Slet en blog");
-            Console.WriteLine("3. Rediger en blog");
+            Console.WriteLine("1. Blog");
             Console.Write("Indsæt dit valg: ");
             int choice = int.Parse(Console.ReadLine());
 
@@ -30,9 +30,11 @@ namespace Dyreinternat_Console
             switch (choice)
             {
                 case 1: // The functions  available for the customer
-                    Console.WriteLine("1. Du kan nu lave et nyt blog indlæg");
-                    Console.WriteLine("2. Vælg den blog du gerne vil slette");
-                    Console.WriteLine("3. Vælg den blog du gerne vil redigere");
+                    Console.WriteLine("1. Se alle blogs");
+                    Console.WriteLine("2. Søg efter en blog");
+                    Console.WriteLine("3. Lav et nyt blog indlæg");
+                    Console.WriteLine("4. Vælg den blog du gerne vil slette");
+                    Console.WriteLine("5. Vælg den blog du gerne vil redigere");
                     Console.Write("Indsæt dit valg: ");
                     choice = int.Parse(Console.ReadLine());
                     switch (choice)
@@ -63,8 +65,28 @@ namespace Dyreinternat_Console
             }
         }
 
+        public void printAllBlogs()  // Methode to print all blogs
+        {
+            List<Blog> blogs = BlogService.GetAll();
+            int i = 1;
+            foreach (Blog blog in blogs)
+            {
+                Console.WriteLine($"\nBlogs {i}");
+                printBlog(blog);
+                i++;
+            }
+        }
 
-
+        public void printBlog(Blog blog) // The methode that prints all the properties of the blogs
+        {
+            Console.WriteLine($"Titel: {blog.Title}");
+            Console.WriteLine($"Beskrivelse: {blog.Description}");
+            Console.WriteLine($"Tid: {blog.DateTime}");
+            Console.WriteLine($"Forfatter: {blog.Author}");
+            Console.WriteLine($"BlogID: {blog.BlogID}");
+          
+           
+        }
 
     }
 }
